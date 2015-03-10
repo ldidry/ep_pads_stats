eb34080bd52b45599c6b31385b723833dfd96561
Luc Didry <luc@didry.org>
2015-03-10 14:50:29 +0100

    Improve blank pads detection

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var eejs       = require('ep_etherpad-lite/node/eejs/'),
     padManager = require('ep_etherpad-lite/node/db/PadManager'),
-    API        = require('ep_etherpad-lite/node/db/API'),
     ERR        = require('ep_etherpad-lite/node_modules/async-stacktrace'),
     async      = require('ep_etherpad-lite/node_modules/async');
+var blankPads = {};
+async.waterfall([
+    function(callback){
+        padManager.listAllPads(callback)
+    },
+    function(data, callback) {
+        for (var pad in data.padIDs) {
+            padManager.getPad(data.padIDs[pad], function(err, data) {
+                if (data.getHeadRevisionNumber() == 0) {
+                    blankPads[data.id] = 1;
+                } else {
+                    delete blankPads[data.id];
+                }
+            });
+        }
+        callback(null, data.padIDs.length);
+    }
+]);
 exports.registerRoute = function (hook_name, args, cb) {
     args.app.get('/stats.json', function(req, res) {
         var pads = [];
@@ -11,19 +28,22 @@ exports.registerRoute = function (hook_name, args, cb) {
             function(callback){
                 padManager.listAllPads(callback)
             },
-            function(data, callback){
-                var timestamp = (new Date).getTime() / 1000 | 0,
-                    padsCount = data.padIDs.length,
-                    blankPads = 0;
-                    for (var pad in data.padIDs) {
-                        API.getRevisionsCount(data.padIDs[pad], function(err, data) {
-                            if (data.revisions == 0) {
-                                blankPads++;
-                            }
-                        });
-                    }
+            function(data, callback) {
+                for (var pad in data.padIDs) {
+                    padManager.getPad(data.padIDs[pad], function(err, data) {
+                        if (data.getHeadRevisionNumber() == 0) {
+                            blankPads[data.id] = 1;
+                        } else {
+                            delete blankPads[data.id];
+                        }
+                    });
+                }
+                callback(null, data.padIDs.length);
+            },
+            function(data, callback) {
+                var timestamp = (new Date).getTime() / 1000 | 0;
                 res.setHeader('Content-Type', 'application/json');
-                res.send('{"timestamp": '+timestamp+', "padsCount": '+padsCount+', "blankPads": '+blankPads+'}');
+                res.send('{"timestamp": '+timestamp+', "padsCount": '+data+', "blankPads": '+Object.keys(blankPads).length+'}');
                 callback();
             }
         ]);
